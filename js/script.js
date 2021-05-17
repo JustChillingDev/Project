b58daa5efd7b5d3426e81b2b378086373aaaff24
QwertyWin <artema210908@gmail.com>
2021-05-17 17:55:57 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+"use sctrict";
+
+console.log('arr' + "- object");
+console.log(4 +"5");
+
+let incr = 10,
+    decr = 10;
+//hidencode
+//incr++;
+//decr--;
+
+console.log(++incr);
+console.log(--decr);
+
+ console.log(5%2);
+
+ console.log(5*5 == 25);
+
+ const isChecked = true,
+       isClose = true;
+
+       console.log(isChecked && isClose);
